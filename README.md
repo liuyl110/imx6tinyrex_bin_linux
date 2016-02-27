b162bbb4b19cc2eabf9f810faaa2bf2671d95b98
Marek Sujak <support@voipac.com>
2016-02-27 19:38:32 +0100

    imx6 tinyrex: Add version 1.1 (generated by yocto)

--- README.md
+++ README.md
@@ -6,40 +6,48 @@ imx6q/dl/s tinyrex linux binaries
     git clone https://github.com/voipac/imx6tinyrex_bin_linux
     cd imx6tinyrex_bin_linux/
     
-# Install linux for imx6s into mmcblk0 card
-    sudo ./fsl-sdcard-partition.sh -s /dev/mmcblk0
+# Install linux for imx6lite into mmcblk0 card
+    sudo ./fsl-sdcard-partition.sh -lite /dev/mmcblk0
 
-# Install linux for imx6dl into mmcblk0 card
-    sudo ./fsl-sdcard-partition.sh -dl /dev/mmcblk0
+# Install linux for imx6basic into mmcblk0 card
+    sudo ./fsl-sdcard-partition.sh -basic /dev/mmcblk0
 
-# Install linux for imx6q into mmcblk0 card
-    sudo ./fsl-sdcard-partition.sh -q /dev/mmcblk0
+# Install linux for imx6pro into mmcblk0 card
+    sudo ./fsl-sdcard-partition.sh -pro /dev/mmcblk0
+
+# Install linux for imx6max into mmcblk0 card
+    sudo ./fsl-sdcard-partition.sh -max /dev/mmcblk0
     
-# Setup u-boot (HDMI)
+# Setup u-boot (imx6lite)
+Imx6 modules will try to reserve 320MB cma memory.
+Because lite module have 256MB of RAM it will require special bootarg settings. 
+    setenv mmcargs "setenv bootargs console=${console},${baudrate} root=${mmcroot} cma=96M"
 
 # Log
-    marek@jessie:~/workdir/imx6tinyrex_bin_linux$ sudo ./fsl-sdcard-partition.sh -s /dev/mmcblk0
+    marek@jessie:~/workdir/imx6/rootfs/imx6tinyrex_bin_linux$ sudo ./fsl-sdcard-partition.sh -lite /dev/sdb
     Selected files:
-    UBOOT:  u-boot-imx6s-tinyrex.imx
-    KERNEL: zImage-imx6s-tinyrex
+    UBOOT  : u-boot-imx6-tinyrexlite.imx
+    KERNEL : zImage-imx6-tinyrexlite.bin
+    FDT    : zImage-imx6-tinyrexlite.dtb
+    ROOT   : core-image-base-imx6-tinyrexlite.tar.bz2
     Creating partitions...
     Partition table destroyed [OK]
     Formating partitions...
     mkfs.fat 3.0.27 (2014-11-12)
     mke2fs 1.42.12 (29-Aug-2014)
-    /dev/mmcblk0p2 contains a ext4 file system labelled 'system'
-        last mounted on /media/tmp on Sun May 24 17:11:09 2015
+    /dev/sdb2 contains a ext4 file system labelled 'system'
+        last mounted on /media/marek/system on Sat Feb 27 18:50:45 2016
     Proceed anyway? (y,n) y
-    Discarding device blocks: done
-    Creating filesystem with 1930240 4k blocks and 483328 inodes
-    Filesystem UUID: e19186c3-400a-4fc8-86fa-758096e5d8f1
+    Creating filesystem with 970610 4k blocks and 242880 inodes
+    Filesystem UUID: 0acc13b5-321a-4ef8-b831-8e6a1090fa5c
     Superblock backups stored on blocks: 
-        32768, 98304, 163840, 229376, 294912, 819200, 884736, 1605632
+        32768, 98304, 163840, 229376, 294912, 819200, 884736
 
     Allocating group tables: done
     Writing inode tables: done
-    Creating journal (32768 blocks): done
+    Creating journal (16384 blocks): done
     Writing superblocks and filesystem accounting information: done 
-    
-    Flashing images...
+
+	Flashing images...
+
 
--- fsl-sdcard-partition.sh
+++ fsl-sdcard-partition.sh
@@ -14,13 +14,16 @@ cat << EOF
 Usage
     $bn <option> device_node
 Example
-    sudo ./fsl-sdcard-partition.sh -q /dev/mmcblk0
+    sudo ./fsl-sdcard-partition.sh -max /dev/mmcblk0
+    sudo ./fsl-sdcard-partition.sh -pro /dev/sdb
+    sudo ./fsl-sdcard-partition.sh -basic /dev/sdb
 
 options:
   -h				displays this help message
-  -s				imx6 solo
-  -dl 				imx6 dual lite
-  -q 				imx6 quad
+  -lite				imx6 lite module (solo)
+  -basic			imx6 basic module (solo)
+  -pro				imx6 pro module (dual)
+  -max 				imx6 max module (quad)
 EOF
 
 }
@@ -36,16 +39,18 @@ function root_test
 
 # parse command line
 moreoptions=1
-node="na"
-imx6s=0
-imx6dl=0
-imx6q=0
+node="invalid"
+imx6lite=0
+imx6basic=0
+imx6pro=0
+imx6max=0
 while [ "$moreoptions" = 1 -a $# -gt 0 ]; do
 	case $1 in
 	    -h) help; exit ;;
-	    -s)  imx6s=1 ;;
-	    -dl) imx6dl=1 ;;
-	    -q)  imx6q=1 ;;
+	    -lite)  imx6lite=1 ;;
+	    -basic)  imx6basic=1 ;;
+	    -pro)    imx6pro=1 ;;
+	    -max)    imx6max=1 ;;
 	    *)  moreoptions=0; node=$1 ;;
 	esac
 	[ "$moreoptions" = 0 ] && [ $# -gt 1 ] && help && exit
@@ -57,6 +62,7 @@ if [ ! -e ${node} ]; then
 	exit
 fi
 
+# detect partition separator
 part=""
 echo ${node} | grep mmcblk > /dev/null
 if [ "$?" -eq "0" ]; then
@@ -69,24 +75,30 @@ FILE_FDT="imx6.dtb"
 FILE_ROOT="core-image-base.tar.bz2"
 function file_finux
 {
-    echo "Selected files:" 
-    if [ "${imx6s}" -eq "1" ]; then
-        FILE_UBOOT="u-boot-imx6s-tinyrex.imx"
-        FILE_KERNEL="zImage-imx6s-tinyrex.bin"
-        FILE_FDT="zImage-imx6s-tinyrex.dtb"
-        FILE_ROOT="core-image-base-imx6s-tinyrex.tar.bz2"
+    echo "Selected files:"
+    if [ "${imx6lite}" -eq "1" ]; then
+        FILE_UBOOT="u-boot-imx6-tinyrexlite.imx"
+        FILE_KERNEL="zImage-imx6-tinyrexlite.bin"
+        FILE_FDT="zImage-imx6-tinyrexlite.dtb"
+        FILE_ROOT="core-image-base-imx6-tinyrexlite.tar.bz2"
     fi
-    if [ "${imx6dl}" -eq "1" ]; then
-        FILE_UBOOT="u-boot-imx6dl-tinyrex.imx"
-        FILE_KERNEL="zImage-imx6dl-tinyrex.bin"
-        FILE_FDT="zImage-imx6dl-tinyrex.dtb"
-        FILE_ROOT="core-image-base-imx6dl-tinyrex.tar.bz2"
+    if [ "${imx6basic}" -eq "1" ]; then
+        FILE_UBOOT="u-boot-imx6-tinyrexbasic.imx"
+        FILE_KERNEL="zImage-imx6-tinyrexbasic.bin"
+        FILE_FDT="zImage-imx6-tinyrexbasic.dtb"
+        FILE_ROOT="core-image-base-imx6-tinyrexbasic.tar.bz2"
     fi
-    if [ "${imx6q}" -eq "1" ]; then
-        FILE_UBOOT="u-boot-imx6q-tinyrex.imx"
-        FILE_KERNEL="zImage-imx6q-tinyrex.bin"
-        FILE_FDT="zImage-imx6q-tinyrex.dtb"
-        FILE_ROOT="core-image-base-imx6q-tinyrex.tar.bz2"
+    if [ "${imx6pro}" -eq "1" ]; then
+        FILE_UBOOT="u-boot-imx6-tinyrexpro.imx"
+        FILE_KERNEL="zImage-imx6-tinyrexpro.bin"
+        FILE_FDT="zImage-imx6-tinyrexpro.dtb"
+        FILE_ROOT="core-image-base-imx6-tinyrexpro.tar.bz2"
+    fi
+    if [ "${imx6max}" -eq "1" ]; then
+        FILE_UBOOT="u-boot-imx6-tinyrexmax.imx"
+        FILE_KERNEL="zImage-imx6-tinyrexmax.bin"
+        FILE_FDT="zImage-imx6-tinyrexmax.dtb"
+        FILE_ROOT="core-image-base-imx6-tinyrexmax.tar.bz2"
     fi
     echo "UBOOT  : ${FILE_UBOOT}"
     echo "KERNEL : ${FILE_KERNEL}"
